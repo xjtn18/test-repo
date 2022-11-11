e4fa53cc286af85c225fb7bd0fdfc6547f12c781
Jacob Nardone <xjtn18@gmail.com>
2022-11-11 14:53:18 -0800

    changed some crap

--- test.py
+++ test.py
@@ -3,10 +3,10 @@
 def main():
     print("Line from local 1")
     print("Line from local 2")
-    print("Local 2 reporting COUNTER")
-    print("hello")
+    print("Local 2 reporting COUNTER NEW")
+    print("hello goodbye")
 
-    a = 5
+    a = 7
 
 
 main()
