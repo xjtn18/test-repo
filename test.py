3709da005f023c636e8758cf46cece045b30debc
Jacob Nardone <xjtn18@gmail.com>
2022-11-11 15:51:45 -0800

    happy jake

--- test.py
+++ test.py
@@ -3,9 +3,10 @@
 def main():
     print("Line from local 1")
     print("Line from local 2")
-    print("STUFF from local 1")
-    print("YYYYYYYYYYYOOOOOOOYYYYYYYYYY")
-    print("oh my..............")
+    print("Local 22 2 22 22 2 2 2 2 2 2 2 2 billion! (2) reporting DUTY")
+    print("HAPPY JAKE")
+    print("pleeeaze")
+    a = 7
 
 
 main()
