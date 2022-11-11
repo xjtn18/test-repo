42f572c5525d99e74182d02ada1c309425593899
Jacob Nardone <xjtn18@gmail.com>
2022-11-11 15:55:54 -0800

    JRAKE

--- test.py
+++ test.py
@@ -3,10 +3,9 @@
 def main():
     print("Line from local 1")
     print("Line from local 2")
-    print("Local 22 2 22 22 2 2 2 2 2 2 2 2 billion! (2) reporting DUTY")
-    print("HAPPY JAKE")
-    print("pleeeaze")
-    a = 7
+    print("STUFF from local 1")
+    print("")
+    print("------###########_----------------")
 
 
 main()
