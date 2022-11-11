94c86e8c238e044c091fd0f55892013ed047c93a
Jacob Nardone <xjtn18@gmail.com>
2022-11-11 14:23:58 -0800

    local 2 counter

--- test.py
+++ test.py
@@ -3,7 +3,10 @@
 def main():
     print("Line from local 1")
     print("Line from local 2")
-    print("Local 2 reporting")
+    print("Local 2 reporting COUNTER")
+    print("hello")
+
+    a = 5
 
 
 main()
