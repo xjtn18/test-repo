05fc8a86b3ce66a9bb470f207ba047334f08701e
Jacob Nardone <xjtn18@gmail.com>
2022-11-12 00:59:34 -0800

    hiya

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 
 
-def main():
+def zain(a : int):
     print("Line from local 1")
     print("Line from local 2")
      
 
+<<<<<<< Updated upstream
     print("The covenant, if it were so easy")
     print("Coogler")
+=======
+    print("ODST")
+>>>>>>> Stashed changes
     print(" :) ")
     print("----- residents evil ?_-  --------")
+    print("----- covenents ?_-  --------")
 
 
-main()
-
+zain(9)
 
 doesnt_exist()
