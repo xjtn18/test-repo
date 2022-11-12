7c973b537bccaeb92dd11008d94c08cdedaef010
Jacob Nardone <xjtn18@gmail.com>
2022-11-12 00:14:48 -0800

    Arbiter
    a description? woah

--- test.py
+++ test.py
@@ -4,11 +4,8 @@ def main():
     print("Line from local 1")
     print("Line from local 2")
      
-    print("STUFF from local 1")
-    print("")
-    print("------!!!!!!!!_----------------")
 
-    print("Coogler")
+    print("The covenant, if it were so easy")
 
 
 main()
